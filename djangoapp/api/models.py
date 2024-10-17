31c1a50a40c5ea8e3b5c06db034ee01abd713d99
Jardel Lion <businessjbx@gmail.com>
2024-10-17 19:21:22 +0200

    update

--- djangoapp/api/models.py
+++ djangoapp/api/models.py
@@ -123,8 +123,7 @@ class Employee(models.Model):
 
 class Notification(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    employee  = models.ForeignKey(Employee, on_delete=models.CASCADE, 
-                                  related_name='notifications')
+    employee  = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='notifications')
     message = models.CharField(max_length=255)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
