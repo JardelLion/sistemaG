3fbfd71a51d2747bfcaea9a9a4fa64e1aa1d280f
Jardel Lion <businessjbx@gmail.com>
2024-10-18 19:13:56 +0200

    added Restrions in nome product to unique=True

--- djangoapp/api/models.py
+++ djangoapp/api/models.py
@@ -14,7 +14,7 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
