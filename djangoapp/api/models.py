ecbde049536d5fec36452a543eca82d0d9127464
Jardel Lion <businessjbx@gmail.com>
2024-10-23 16:04:55 +0200

    added product history __str__

--- djangoapp/api/models.py
+++ djangoapp/api/models.py
@@ -34,6 +34,9 @@ class ProductHistory(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     acquisition_value = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) 
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Histórico do produto: {self.name}"
    
 
 class Employee(models.Model):
