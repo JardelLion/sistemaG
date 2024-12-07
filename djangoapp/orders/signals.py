3192d3d1faadd70b91c6e88c2842339507eb8ded
Jardel Lion <businessjbx@gmail.com>
2024-12-07 13:17:09 +0200

    makemigrations

--- djangoapp/orders/signals.py
+++ djangoapp/orders/signals.py
@@ -6,5 +6,6 @@ def create_default_stock(sender, **kwargs):
     if not StockReference.objects.filter(name='Estoque Padrão').exists():
         StockReference.objects.create(
             name='Estoque Padrão',
+            is_active=True,
             description='Este é o estoque criado automaticamente pelo sistema.'
         )
